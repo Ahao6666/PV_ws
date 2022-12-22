a758e24d5c17b703c189a62c017419da36444114
Ahao6666 <ahousemail@163.com>
2022-12-22 09:42:00 +0800

    :construction: TCP test ok

--- src/landing_leg/src/landing_leg_tcp_client.cpp
+++ src/landing_leg/src/landing_leg_tcp_client.cpp
@@ -24,50 +24,51 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	
 	char recvbuf[BUF_SIZE];
-	
-	/*
-	 *@fuc: socket()创建套节字
-	 *
-	 */
-	int socket_cli = socket(AF_INET, SOCK_STREAM, 0);
-	if(socket_cli < 0)
+    while(ros::ok)
 	{
-		std::cout << "socket() error\n";
-		return -1;
-	}
-	
-	/*
-	 *@fuc: 服务器端IP4地址信息,struct关键字可不写
-	 *@fuc: 初始化sever地址信息   
-	 */
-	struct sockaddr_in sev_addr;  
-	memset(&sev_addr, 0, sizeof(sev_addr));
-	sev_addr.sin_family      = AF_INET;
-	sev_addr.sin_port        = htons(MYPORT);
-	sev_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-	std::cout << "connecting..." << std::endl;
-	/*
-	 *@fuc: 使用connect()函数来配置套节字,建立一个与TCP服务器的连接
-	 */
-	if(connect(socket_cli, (struct sockaddr*) &sev_addr, sizeof(sev_addr)) < 0)
-	{ 
-		std::cout << "connect error" << std::endl;
-		return -1;
-	}
-	else
-		std::cout << "connected successfullly!" << std::endl;
+        /*
+        *@fuc: socket()创建套节字
+        *
+        */
+        int socket_cli = socket(AF_INET, SOCK_STREAM, 0);
+        if(socket_cli < 0)
+        {
+            std::cout << "socket() error\n";
+            return -1;
+        }
+        
+        /*
+        *@fuc: 服务器端IP4地址信息,struct关键字可不写
+        *@fuc: 初始化sever地址信息   
+        */
+        struct sockaddr_in sev_addr;  
+        memset(&sev_addr, 0, sizeof(sev_addr));
+        sev_addr.sin_family      = AF_INET;
+        sev_addr.sin_port        = htons(MYPORT);
+        sev_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+        std::cout << "connecting..." << std::endl;
+        /*
+        *@fuc: 使用connect()函数来配置套节字,建立一个与TCP服务器的连接
+        */
+        if(connect(socket_cli, (struct sockaddr*) &sev_addr, sizeof(sev_addr)) < 0)
+        { 
+            std::cout << "connect error" << std::endl;
+            return -1;
+        }
+        else
+            std::cout << "connected successfullly!" << std::endl;
 		
-	while(ros::ok)
-	{
+
 		/*
 		 *@fuc: 使用recv()函数来接收服务器发送的消息
 		 */
 		recv(socket_cli, recvbuf, sizeof(recvbuf), 0);
 		printf("server message: %s\n", recvbuf);
+        /*
+        *@fuc: 关闭连接
+        */
+        close(socket_cli);
 	}
-	/*
-	 *@fuc: 关闭连接
-	 */
-	close(socket_cli);
+
 	return 0;
 }
--- src/landing_leg/src/landing_leg_tcp_server.cpp
+++ src/landing_leg/src/landing_leg_tcp_server.cpp
@@ -7,17 +7,27 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <geometry_msgs/PoseStamped.h>
+
 #define PORT 8080    //端口号
 #define LOG  1       //请求队列中最大连接数量
  
 using namespace std;
  
- 
+// Get local position
+geometry_msgs::PoseStamped local_position;
+void local_position_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    local_position = *msg;
+}
 int main (int argc, char** argv)
 {
+    
 	ros::init(argc, argv, "server_node");
 	ros::NodeHandle nh;
-	
+
+    // local position callback function
+    ros::Subscriber local_position_sub_ = nh.subscribe<geometry_msgs::PoseStamped>
+            ("mavros/local_position/pose", 10, local_position_cb);
 	/*
 	 *@fuc: 监听套节字描述符和连接套节字描述符
 	 *@fuc; 服务器端和客户端IP4地址信息,struct关键字可不要
@@ -26,10 +36,9 @@ int main (int argc, char** argv)
 	struct sockaddr_in sever;
 	struct sockaddr_in client;
 	socklen_t addrlen;
-	
 	/*
-	 *@fuc: 使用socket()函数产生套节字描述符
-	 */
+	*@fuc: 使用socket()函数产生套节字描述符
+	*/
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(listenfd == -1)
 	{
@@ -38,38 +47,41 @@ int main (int argc, char** argv)
 	}
 	
 	/*
-	 *@fuc: 初始化server套节字地址信息 
-	 */
+	*@fuc: 初始化server套节字地址信息 
+	*/
 	memset((void *)&sever,0,sizeof(sever));
 	sever.sin_family = AF_INET;
 	sever.sin_addr.s_addr = htonl(INADDR_ANY);
 	sever.sin_port = htons(PORT);
- 
- 	/*
-	 *@fuc: 用bind()函数，将套接字与指定的协议地址绑定 
-	 */
+	/*
+	*@fuc: 用bind()函数，将套接字与指定的协议地址绑定 
+	*/
 	if(bind(listenfd,(struct sockaddr *)&sever,sizeof(sever)) < 0)
 	{
 		printf("bind() error\n");
 		return -1;
 	}
 	
- 	/*
-	 *@fuc: 使用listen()函数，等待客户端的连接 
-	 */
-    if(listen(listenfd, LOG) < 0)
-    {
-        printf("listen() error.\n");
-        return -1;
-    }
-    
-    addrlen = sizeof(client);
-    int count = 0;
-    char send_data[20];
+	/*
+	*@fuc: 使用listen()函数，等待客户端的连接 
+	*/
+	if(listen(listenfd, LOG) < 0)
+	{
+		printf("listen() error.\n");
+		return -1;
+	}
+	
+	addrlen = sizeof(client);
+	int count = 0;
+	char send_data[20];
+	ros::Rate rate(2.0);
+
+	// get UAV local position altitude
+	double position_z = 0;
     //不断监听客户端请求
 	while(ros::ok)
 	{
-		connectfd = accept(listenfd,(struct sockaddr *)&client,&addrlen);
+        connectfd = accept(listenfd,(struct sockaddr *)&client,&addrlen);
 		if(connectfd < 0)
 		{
 			printf("connect() error \n");
@@ -77,9 +89,20 @@ int main (int argc, char** argv)
 		}
 		printf("You got a connection from client's IP is %s, port is %d\n",
 				inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-        string s = "hello" + to_string(count++);
-        strcpy(send_data,s.data());
-		send(connectfd, send_data,8,0);
+        // get the UAV pose
+        position_z = local_position.pose.position.z;
+		std::cout << "local hight:" <<position_z<< std::endl;
+        if(position_z < 1.0){
+			cout<<"Horizontal Plane landing mode"<<endl;
+			send(connectfd, "1",40,0);
+        }else{
+			// string s = "Inclined Plane landing mode" + to_string(count++);
+			// strcpy(send_data,send_data_str.data());
+			cout<<"Inclined Plane landing mode"<<endl;
+			send(connectfd, "2",40,0);
+        }
+		ros::spinOnce();
+        rate.sleep();
 		close(connectfd);
 	}
 	close(listenfd);
