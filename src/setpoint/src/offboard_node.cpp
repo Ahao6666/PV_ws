16fba72464b967ef3ccf789b78991666db78d578
Ahao6666 <ahousemail@163.com>
2023-01-12 17:06:46 +0800

    :bookmark: offboard mission fly above PV_car

--- src/setpoint/src/offboard_node.cpp
+++ src/setpoint/src/offboard_node.cpp
@@ -19,6 +19,11 @@ geometry_msgs::PoseStamped local_position;
 void local_position_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
     local_position = *msg;
 }
+// Get pv car position
+geometry_msgs::PoseStamped pv_car_position;
+void pv_car_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    pv_car_position = *msg;
+}
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "offb_node");
@@ -35,6 +40,8 @@ int main(int argc, char **argv)
     // local position callback function
     ros::Subscriber local_position_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>
             ("mavros/local_position/pose", 10, local_position_cb);
+    ros::Subscriber lpv_car_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>
+            ("mavros/pv_car/pose", 10, pv_car_cb);
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
@@ -45,9 +52,9 @@ int main(int argc, char **argv)
     }
 
     geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = local_position.pose.position.x;
-    pose.pose.position.y = local_position.pose.position.y;
-    pose.pose.position.z = local_position.pose.position.z;
+    pose.pose.position.x = pv_car_position.pose.position.x;
+    pose.pose.position.y = pv_car_position.pose.position.y;
+    pose.pose.position.z = pv_car_position.pose.position.z + 2;
 
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
@@ -65,27 +72,12 @@ int main(int argc, char **argv)
     // ros::Time last_request = ros::Time::now();
 
     while(ros::ok()){
-        // if( current_state.mode != "OFFBOARD" &&
-        //     (ros::Time::now() - last_request > ros::Duration(5.0))){
-        //     if( set_mode_client.call(offb_set_mode) &&
-        //         offb_set_mode.response.mode_sent){
-        //         ROS_INFO("Offboard enabled");
-        //     }
-        //     last_request = ros::Time::now();
-        // } else {
-        //     if( !current_state.armed &&
-        //         (ros::Time::now() - last_request > ros::Duration(5.0))){
-        //         if( arming_client.call(arm_cmd) &&
-        //             arm_cmd.response.success){
-        //             ROS_INFO("Vehicle armed");
-        //         }
-        //         last_request = ros::Time::now();
-        //     }
-        // }
+
         double run_time = ros::Time::now().toSec();
-        pose.pose.position.x = 1.5 * sin(0.25 * run_time);
-        pose.pose.position.y = 1.5 * cos(0.25 * run_time);
-        pose.pose.position.z = local_position.pose.position.z;
+        pose.pose.position.x = pv_car_position.pose.position.x;
+        pose.pose.position.y = pv_car_position.pose.position.y;
+        pose.pose.position.z = pv_car_position.pose.position.z + 2;
+        ROS_INFO("setpoint x=%f,y=%f,z=%f",pose.pose.position.x, pose.pose.position.y, pose.pose.position.z);
         local_pos_pub.publish(pose);
 
         ros::spinOnce();
